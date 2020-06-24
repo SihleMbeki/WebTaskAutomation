e3cc675c94221dc88c19b67b96258dafdd5c6f2f
Masakhane Matanzima <masakhane.matanzima@santam.co.za>
2020-06-24 23:33:58 +0200

    Utilities

---
+++ src/test/java/utilities/Listener.java
@@ -0,0 +1,69 @@
+package utilities;
+
+import java.io.IOException;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.Markup;
+
+import testing.Base;
+
+public class Listener  extends Base implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Report.test.log(Status.FAIL, "Test Failed");
+		try {
+			Report.test.fail("Screenshot",MediaEntityBuilder.createScreenCaptureFromPath(Base.driver.screenShot()).build());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if(Base.driver.getInstance()!=null) {
+			Base.driver.getInstance().close();
+		}
+		
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/utilities/WebDriverInstance.java
@@ -0,0 +1,133 @@
+package utilities;
+
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverInstance {
+	private WebDriver driver;
+
+	public WebDriverInstance() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	public WebDriver getInstance() {
+		return driver;
+	}
+
+	public void closeDriver() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
+	public void goTo(String url) {
+		driver.get(url);
+	}
+
+	public boolean selectDropdown(WebElement select, String option) {
+		try {
+			Select selection = new Select(select);
+			selection.selectByVisibleText(option);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public boolean clickElementByLocator(String xpath) {
+		try {
+			driver.findElement(By.xpath(xpath)).click();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public boolean clickElementByCssLocator(String css) {
+		try {
+			driver.findElement(By.cssSelector(css)).click();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public boolean enterText(String css,String text) {
+		try {
+			driver.findElement(By.cssSelector(css)).sendKeys(text);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public WebElement findElementByCssLocator(String css) {
+		try {
+			return driver.findElement(By.cssSelector(css));
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+			return null;
+		}
+	}
+	public  boolean findElementByXpath(String xpath) {
+		try {
+			driver.findElement(By.xpath(xpath));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean waitForVisibilityOfElement(String xpath) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver,10);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean waitForPresenceOfElement(String xpath) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver,10);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public String screenShot() {
+		try {
+			Date date=new Date();
+			String fileName=Report.reportPath+date.toString().replace(":", "_").replace(" ", "")+".jpg";
+			File screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(fileName));
+			return fileName;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
